81716b4fc0ed7a27f58080f26b351f991eee00c9
rhythmitaliya <ritaliya2003@gmail.com>
2025-11-10 13:44:45 +0530

    10 Nov

--- client/next.config.ts
+++ client/next.config.ts
@@ -8,6 +8,26 @@ const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'src', 'styles')],
   },
+  images: {
+    // Allow optimized loading of remote images used in website pages/components
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'adventor.wpengine.com',
+        pathname: '/wp-content/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'html-templates.evonicmedia.com',
+        pathname: '/adventurist/assets/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
